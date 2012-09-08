72a81b3440c8e9c08cd3e67d8b35808c820ce6a6
Alan Kligman <alan.kligman@gmail.com>
2012-09-08 11:20:34 -0400

    Vector constructor tests.

---
+++ tests/specs/vector-constructor.spec.js
@@ -0,0 +1,28 @@
+var M = require("src/m");
+
+describe("Vector", function() {
+  describe("with dimensions", function() {
+    var v;
+    beforeEach(function() {
+      v = new M.Vector(2);
+    });
+    it("should create a new vector", function() {
+      expect(v).toBeDefined();
+    });
+    it("should initialize new vector to 0", function() {
+      expect(v).toBeLike([0, 0]);
+    });
+  });
+  describe("with initializer", function() {
+    var v;
+    beforeEach(function() {
+      v = new M.Vector([1, 2, 3, 4]);
+    });
+    it("should create a new matrix", function() {
+      expect(v).toBeDefined();
+    });
+    it("should initialize the new matrix with values", function() {
+      expect(v).toBeLike([1, 2, 3, 4]);
+    });
+  });
+});
